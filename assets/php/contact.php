f8ef888d02d445c8583e7f8548b894e86443e321
Michael <rodmike751@gmail.com>
2023-10-02 16:37:23 +0000

    firstchanges

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,5 +1,5 @@
 <?php
- $to = "support@rixrod.com.com";
+ $to = "info@axoninfosystems.com";
  $subject = $_POST['subject'];
  $body = "From, ".$_POST['name']."\n\n"."Email, ".$_POST['email']."\n\n".$_POST['message'];
  if (mail($to, $subject, $body)) {
